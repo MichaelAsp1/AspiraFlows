8637593d45c0685987027ef2de78fd8023c93af0
MichaelAsp1 <master.aspegren@gmail.com>
2025-11-19 16:46:54 +0100

    layout

--- projects/mysite/app/(app)/layout.js
+++ projects/mysite/app/(app)/layout.js
@@ -3,9 +3,9 @@ import Footer from "../../components/Footer";
 
 export default function AppLayout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-br from-purple-50/40 via-white to-indigo-50/40 text-gray-900">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 w-full">
         {children}
       </main>
       <Footer />
